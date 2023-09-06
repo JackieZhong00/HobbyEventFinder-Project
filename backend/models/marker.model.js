c37d6a873135ddbd6b4def22021e91fdb7369e2e
jackie zhong <jackiery.zhong@gmail.com>
2023-09-05 23:55:21 -0700

    started to update

---
+++ backend/models/marker.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const markerSchema = new Schema({
+  lat: {
+    type: Number,
+    required: true,
+  },
+  lng: {
+    type: Number,
+    required: true,
+  },
+})
+
+const Marker = mongoose.model('Marker', markerSchema)
+
+module.exports = Marker
